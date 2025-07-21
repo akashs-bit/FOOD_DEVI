1448fe9c07e98e1c484ecc5999955482f389f9e3
Prasad-Bojja <prasadbojja5@gmail.com>
2025-07-21 19:44:05 +0530

    2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
 import Verify from "./pages/verify/verify";
-import Myorders from "./pages/myOrders/MyOrders.jsx";
+import Myorders from "./pages/myOrders/MyOrders";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
